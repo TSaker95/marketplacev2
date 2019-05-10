17a06a7d2b17a19700b0472df6dae66fb1cea6fe
TSaker95 <torryarno@hotmail.com>
2019-05-10 14:24:21 +1000

    always the gemfile

--- test/models/listing_test.rb
+++ test/models/listing_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class ListingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+end
+
+test "should not save listing without title" do
+  Listing = Listing.new
+  assert !listing.save
 end
